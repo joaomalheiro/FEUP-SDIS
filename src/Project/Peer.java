40713f4c3c68aefe2098568870d068a16d955b73
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2019-03-20 10:08:51 +0000

    initial commit with project stucture

---
+++ src/Project/Peer.java
@@ -0,0 +1,19 @@
+package Project;
+
+public class Peer {
+
+    private String peerId;
+    private String peerAcessPoint;
+
+    public static void main(String args[]) {
+
+        initPeer(args);
+
+
+    }
+
+    private static void initPeer(String args[]){
+
+    }
+
+}
