e42538b9c15699d416f080821f4c4ac4a592c987
up201603526@FEUPsig.fe.up.pt <up201603526@FEUPsig.fe.up.pt>
2019-03-27 11:47:36 +0000

    MultiCast Working

--- SDIS/src/Peer.java
+++ SDIS/src/Peer.java
@@ -2,15 +2,32 @@ import java.io.IOException;
 
 public class Peer {
 
+    private static int serverId;
     private String peerId;
-    private String peerAcessPoint;
+    private static String peerAcessPoint;
+    private static String protocolVersion;
     private static  MultiCast MC;
 
     public static void main(String args[]) throws IOException {
 
-        MC = new MultiCast("230.0.0.0","4446");
+        initAtributes(args);
+        MC = new MultiCast(args[0],args[1]);
+        if(args.length == 2) {
+            MC.run();
+        }
+        if(args.length == 3) {
+            if("sender".equals(args[2])){
+                MC.sendMsg("end".getBytes());
+            }
+        }
 
 
+    }
+
+    private static void initAtributes(String[] args) {
+        protocolVersion = args[0];
+        serverId = Integer.parseInt(args[1]);
+        peerAcessPoint = args[2];
     }
 
     private static void initPeer(String args[]){
